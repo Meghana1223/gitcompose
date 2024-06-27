dd933afb34f84c2f74cc9cd0c57df9a2486a7dca
Meghana1223 <meghanamahamkali@gmail.com>
2024-06-27 12:01:48 +0530

    Delete web.sh
    XX

--- web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo yum update -y 
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1> Welcome to webpage </h1>" > /var/www/http/index.html
-# <h1> Welcome to webpage </h1> content is storeing in the index.html file
